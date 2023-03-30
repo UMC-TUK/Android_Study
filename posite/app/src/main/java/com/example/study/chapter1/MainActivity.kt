3135c9e59596b8bf7f3ed955e05893f1fa7ceed0
posite <su84053@gmail.com>
2023-03-30 17:03:50 +0900

    폴더 이름 변경 및 1주차 실습

---
+++ posite/app/src/main/java/com/example/study/chapter1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.study.chapter1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.study.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
