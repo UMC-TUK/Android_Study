ff66ee005100f5d32d7326ff1936850e7b3e4de4
Minsu <comst19@naver.com>
2023-04-28 17:40:26 +0900

    Chapter3 미션

--- 컴쓰트/ActivityFragment/app/src/main/java/com/comst19/activityfragment/TwoActivity.kt
+++ 컴쓰트/ActivityFragment/app/src/main/java/com/comst19/activityfragment/TwoActivity.kt
@@ -1,5 +1,6 @@
 package com.comst19.activityfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,6 +16,20 @@ class TwoActivity : AppCompatActivity() {
         binding = ActivityTwoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tv.text = intent.getStringExtra("data")
         Toast.makeText(this,intent.getStringExtra("data"), Toast.LENGTH_LONG).show()
+
+        binding.bakcBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("back","Back")
+            setResult(100,intent)
+            finish()
+        }
+
+        binding.goBtn.setOnClickListener {
+            val intent = Intent(this, ThreeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
