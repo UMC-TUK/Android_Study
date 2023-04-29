cdc9dec697cb9e1a5646e5d3350c40f5a8acd28d
hangunhee39 <ghee0831@naver.com>
2023-04-29 20:56:32 +0900

    ch_mission

--- 건끼/ch03_mission/app/src/main/java/com/example/ch03_mission/MainActivity.kt
+++ 건끼/ch03_mission/app/src/main/java/com/example/ch03_mission/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.ch03_mission
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.ch03_mission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
+    val activityTwoLauncher : ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == RESULT_OK){
+            Toast.makeText(this, "${it.data?.getStringExtra("finish")}",Toast.LENGTH_LONG).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "Activity 1"
+
+        binding.button.setOnClickListener {
+            intent = Intent(this, TwoActivity::class.java)
+            intent.putExtra("edit",binding.edit.text.toString())
+            activityTwoLauncher.launch(intent)
+        }
     }
 }
