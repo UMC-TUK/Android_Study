0f7f9a4bbdb0ef64f6a6e1627f75b2112b4b448d
hangunhee39 <ghee0831@naver.com>
2023-05-17 20:23:48 +0900

    ch07_mission

--- 건끼/ch07_mission/app/src/main/java/com/example/ch07_mission/MainActivity.kt
+++ 건끼/ch07_mission/app/src/main/java/com/example/ch07_mission/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.example.ch07_mission
 
+import android.content.Intent
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.example.ch07_mission.databinding.ActivityMainBinding
+import java.lang.String.format
+
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var timer = 0
+    private val soundPool = SoundPool.Builder().build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+
+        binding.startBtn.setOnClickListener {
+            if (timer ==0) return@setOnClickListener
+            binding.startBtn.isGone =true
+            thread{
+                while (timer >0){
+                    sleep(1000)
+                    timer -=1
+                    runOnUiThread {
+                          binding.min.text = format(getString(R.string.min),timer/60)
+                          binding.sec.text = format(getString(R.string.sec),timer%60)
+                    }
+                }
+                runOnUiThread {
+                    timer =0
+                    binding.sec.text = format(getString(R.string.sec),0)
+                    binding.startBtn.isVisible =true
+                    bellSoundId?.let { soundId ->
+                        soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+                    }
+                }
+            }
+        }
+        binding.b10.setOnClickListener {
+            timer +=10
+            binding.min.text = format(getString(R.string.min),timer/60)
+            binding.sec.text = format(getString(R.string.sec),timer%60)
+        }
+        binding.b30.setOnClickListener {
+            timer +=30
+            binding.min.text = format(getString(R.string.min),timer/60)
+            binding.sec.text = format(getString(R.string.sec),timer%60)
+        }
+        binding.b60.setOnClickListener {
+            timer +=60
+            binding.min.text = format(getString(R.string.min),timer/60)
+            binding.sec.text = format(getString(R.string.sec),timer%60)
+        }
+        binding.reset.setOnClickListener {
+            timer =0
+            binding.min.text = format(getString(R.string.min),timer/60)
+            binding.sec.text = format(getString(R.string.sec),timer%60)
+        }
+        binding.MusicBtn.setOnClickListener {
+            intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+
 }
---
+++ 건끼/ch07_mission/app/src/main/java/com/example/ch07_mission/MainActivity2.kt
@@ -0,0 +1,64 @@
+package com.example.ch07_mission
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import com.example.ch07_mission.databinding.ActivityMain2Binding
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding :ActivityMain2Binding
+    var player : MediaPlayer? =null
+    var isPlaying = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playControlImageView.setOnClickListener {
+            if(player==null) {
+                player = MediaPlayer.create(this, R.raw.koreasong)
+                binding.playerSeekBar.max = player!!.duration
+            }
+            if(!isPlaying) {
+                isPlaying=true
+                player?.start()
+                var min = player!!.duration/60000
+                var sec = (player!!.duration-60000*min)/1000
+                binding.totalTimeTextView.text ="$min:$sec"
+                binding.playControlImageView.setImageResource(R.drawable.baseline_pause_24)
+                thread{
+                    while (isPlaying){
+                        runOnUiThread {
+                            if (player==null) return@runOnUiThread
+                            binding.playTimeTextView.text =
+                                "${player!!.currentPosition/60000}:${ (player!!.currentPosition-60000*(player!!.currentPosition/60000))/1000 }"
+                            binding.playerSeekBar.progress =player!!.currentPosition
+                        }
+                        sleep(1000)
+                    }
+                }
+            }else{
+                player?.pause()
+                binding.playControlImageView.setImageResource(R.drawable.baseline_play_arrow_24)
+                isPlaying=false
+            }
+        }
+        binding.playStopImageVIew.setOnClickListener {
+            player?.stop()
+            player =null
+            isPlaying = false
+            binding.playControlImageView.setImageResource(R.drawable.baseline_play_arrow_24)
+            binding.playerSeekBar.progress =0
+            binding.playTimeTextView.text="0:0"
+        }
+
+    }
+
+}
