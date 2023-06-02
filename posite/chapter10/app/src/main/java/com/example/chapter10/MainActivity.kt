e8099616ef3a472a31ebcb98be6b24fa2e6f08c7
posite <su84053@gmail.com>
2023-06-02 15:52:00 +0900

    10주차 카카오 로그인 추가

--- posite/chapter10/app/src/main/java/com/example/chapter10/MainActivity.kt
+++ posite/chapter10/app/src/main/java/com/example/chapter10/MainActivity.kt
@@ -5,8 +5,15 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.chapter10.api.APIRepository
 import com.example.chapter10.databinding.ActivityMainBinding
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.model.User
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -19,7 +26,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        var keyHash = Utility.getKeyHash(this)
+        KakaoSdk.init(this, "8eb469370f02128498f483cfd0819d18")
+
+        Log.d("hash", "keyHash: $keyHash")
         setContentView(binding.root)
+        binding.buttonKakaoLogin.setOnClickListener {
+            lifecycleScope.launch {
+                var kakaoUser: User? = null
+                try {
+                    // 서비스 코드에서는 간단하게 로그인 요청하고 oAuthToken 을 받아올 수 있다.
+                    val oAuthToken = UserApiClient.loginWithKakao(applicationContext)
+                    Log.d("MainActivity", "beanbean > $oAuthToken")
+                    UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+                        if (error != null) {
+                            Log.e("user", "토큰 정보 보기 실패", error)
+                        }
+                        else if (tokenInfo != null) {
+                            Log.i("user", "토큰 정보 보기 성공" +
+                                    "\n회원번호: ${tokenInfo.id}" +
+                                    "\n만료시간: ${tokenInfo.expiresIn} 초")
+                        }
+                    }
+                    UserApiClient.instance.me { user, error ->
+                        if (error != null) {
+                            Log.e("user", "사용자 정보 요청 실패", error)
+                        }
+                        else if (user != null) {
+                            kakaoUser = user
+                            Log.i("user", "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n이메일: ${user.kakaoAccount?.email}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                            val intent = Intent(applicationContext, ServiceActivity::class.java)
+                            intent.putExtra("kakao",kakaoUser)
+                            startActivity(intent)
+                        }
+                    }
+                } catch (error: Throwable) {
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        Log.d("MainActivity", "${error.message} 사용자가 명시적으로 취소")
+                    } else {
+                        Log.e("MainActivity", "인증 에러 발생", error)
+                    }
+                }
+            }
+        }
         binding.goAuth.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${BuildConfig.baseUrl}authorize?client_id=${BuildConfig.id}"))
             startActivity(intent)
@@ -31,6 +84,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 
     override fun onResume() {
         super.onResume()
@@ -68,9 +122,11 @@ class MainActivity : AppCompatActivity() {
             }.await()
             if(result != null) {
                 Log.d("result", "id: ${result.id},  email: ${result.email}")
+                Log.d("result", "url: ${result.avatar_url}")
                 val intent = Intent(applicationContext, ServiceActivity::class.java)
                 intent.putExtra("id", result.name)
                 intent.putExtra("url", result.html_url)
+                intent.putExtra("profile", result.avatar_url)
                 startActivity(intent)
             }
         }
--- posite/chapter10/app/src/main/java/com/example/chapter10/ServiceActivity.kt
+++ posite/chapter10/app/src/main/java/com/example/chapter10/ServiceActivity.kt
@@ -1,11 +1,16 @@
 package com.example.chapter10
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.bumptech.glide.Glide
 import com.example.chapter10.databinding.ActivityServiceBinding
+import com.kakao.sdk.user.model.User
 
 class ServiceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityServiceBinding
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityServiceBinding.inflate(layoutInflater)
@@ -15,6 +20,16 @@ class ServiceActivity : AppCompatActivity() {
         }
         intent.getStringExtra("url")?.let {
             binding.userAddress.text = it
+        }
+        intent.getStringExtra("profile")?.let {
+            Glide.with(binding.profileImg).load(it)
+                .into(binding.profileImg)
+        }
+        intent.getParcelableExtra("kakao", User::class.java)?.let {
+            binding.userId.text = it.kakaoAccount?.profile?.nickname
+            binding.userAddress.text = it.kakaoAccount?.email
+            Glide.with(binding.profileImg).load(it.kakaoAccount?.profile?.thumbnailImageUrl)
+                .into(binding.profileImg)
         }
 
     }
